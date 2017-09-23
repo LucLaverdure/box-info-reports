6f4dff1b5d68cd60e6531d8b4bdb53d1cd780c5f
contact@luclaverdure.com <contact@luclaverdure.com>
2017-09-23 18:22:23 -0400

    Minor style changes

--- wp-content/themes/bash/footer.php
+++ wp-content/themes/bash/footer.php
@@ -2,11 +2,11 @@
 <hr />
 <div id="footer">
 <!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way, it's our only promotion or advertising. -->
-	<p>_______________________________________
+	<hr />
 	<br />
 		<a href="/"><?php bloginfo('name'); ?></a>
-		<a href="https://github.com/LucLaverdure/box-info">GitHub (Client Tools Here)</a>
-		<a href="https://github.com/LucLaverdure/box-info-reports">GitHub (Server Tools Here)</a>
+		<a target="_blank" href="https://github.com/LucLaverdure/box-info">GitHub (Client Tools Here)</a>
+		<a target="_blank" href="https://github.com/LucLaverdure/box-info-reports">GitHub (Server Tools Here)</a>
 	</p>
 </div>
 </div>
