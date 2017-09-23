9fb126c9d4d92cfcd419f4b1f3af5beeef1f9667
contact@luclaverdure.com <contact@luclaverdure.com>
2017-09-23 19:10:05 -0400

    ASCII Funk fun

--- wp-content/themes/bash/header.php
+++ wp-content/themes/bash/header.php
@@ -20,9 +20,32 @@
 
 
 <div id="header">
+<pre style="color:#00ff00;">
+<a href="/"  class="initial">
+RRRRRRRRRRRRRRRRR                     kkkkkkkk                                SSSSSSSSSSSSSSS DDDDDDDDDDDDD       KKKKKKKKK    KKKKKKK
+R::::::::::::::::R                    k::::::k                              SS:::::::::::::::SD::::::::::::DDD    K:::::::K    K:::::K
+R::::::RRRRRR:::::R                   k::::::k                             S:::::SSSSSS::::::SD:::::::::::::::DD  K:::::::K    K:::::K
+RR:::::R     R:::::R                  k::::::k                             S:::::S     SSSSSSSDDD:::::DDDDD:::::D K:::::::K   K::::::K
+  R::::R     R:::::Ruuuuuu    uuuuuu   k:::::k    kkkkkkkuuuuuu    uuuuuu  S:::::S              D:::::D    D:::::DKK::::::K  K:::::KKK
+  R::::R     R:::::Ru::::u    u::::u   k:::::k   k:::::k u::::u    u::::u  S:::::S              D:::::D     D:::::D K:::::K K:::::K   
+  R::::RRRRRR:::::R u::::u    u::::u   k:::::k  k:::::k  u::::u    u::::u   S::::SSSS           D:::::D     D:::::D K::::::K:::::K    
+  R:::::::::::::RR  u::::u    u::::u   k:::::k k:::::k   u::::u    u::::u    SS::::::SSSSS      D:::::D     D:::::D K:::::::::::K     
+  R::::RRRRRR:::::R u::::u    u::::u   k::::::k:::::k    u::::u    u::::u      SSS::::::::SS    D:::::D     D:::::D K:::::::::::K     
+  R::::R     R:::::Ru::::u    u::::u   k:::::::::::k     u::::u    u::::u         SSSSSS::::S   D:::::D     D:::::D K::::::K:::::K    
+  R::::R     R:::::Ru::::u    u::::u   k:::::::::::k     u::::u    u::::u              S:::::S  D:::::D     D:::::D K:::::K K:::::K   
+  R::::R     R:::::Ru:::::uuuu:::::u   k::::::k:::::k    u:::::uuuu:::::u              S:::::S  D:::::D    D:::::DKK::::::K  K:::::KKK
+RR:::::R     R:::::Ru:::::::::::::::uuk::::::k k:::::k   u:::::::::::::::uuSSSSSSS     S:::::SDDD:::::DDDDD:::::D K:::::::K   K::::::K
+R::::::R     R:::::R u:::::::::::::::uk::::::k  k:::::k   u:::::::::::::::uS::::::SSSSSS:::::SD:::::::::::::::DD  K:::::::K    K:::::K
+R::::::R     R:::::R  uu::::::::uu:::uk::::::k   k:::::k   uu::::::::uu:::uS:::::::::::::::SS D::::::::::::DDD    K:::::::K    K:::::K
+RRRRRRRR     RRRRRRR    uuuuuuuu  uuuukkkkkkkk    kkkkkkk    uuuuuuuu  uuuu SSSSSSSSSSSSSSS   DDDDDDDDDDDDD       KKKKKKKKK    KKKKKKK
+</a>
+</pre>
 	<div id="headerimg">
 		<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
 		<div class="description"><?php bloginfo('description'); ?></div>
+		<a target="_blank" href="/">Home</a>
+		<a target="_blank" href="https://github.com/LucLaverdure/box-info">GitHub (Client Tools Here)</a>
+		<a target="_blank" href="https://github.com/LucLaverdure/box-info-reports">GitHub (Server Tools Here)</a>
 	</div>
 </div>
 <hr />
